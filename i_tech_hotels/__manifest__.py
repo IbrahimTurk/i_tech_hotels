b83ae8f99041c804c89c49884351c2a115cb4fc2
Ibrahim M <89030061+IbrahimTurk@users.noreply.github.com>
2022-06-05 13:08:41 +0300

    Update __manifest__.py

--- i_tech_hotels/__manifest__.py
+++ i_tech_hotels/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name' : 'Hotels',
+    'name' : 'I-Tech Hotels',
     'version' : '15.0',
     'summary': 'for Hotels',
     'sequence': 10,
